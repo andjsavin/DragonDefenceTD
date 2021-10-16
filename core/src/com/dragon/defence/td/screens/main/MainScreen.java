ad7b6425135c19d3dfa54cb9bf72be74500c4390
andj <andj.savin@gmail.com>
2021-10-16 12:42:16 +0300

    added settings functions

--- core/src/com/dragon/defence/td/screens/main/MainScreen.java
+++ core/src/com/dragon/defence/td/screens/main/MainScreen.java
@@ -2,6 +2,7 @@ package com.dragon.defence.td.screens.main;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
@@ -17,8 +18,10 @@ public class MainScreen implements IFont, InputProcessor {
     Button newGameButton, settingsButton, chooseButton;
     BigDragon bigDragon;
     Music bigDragonFlap;
-    long flapId;
-    boolean flapping = false;
+    boolean settings = false, chooseLevel = false;
+    float sound = 1.0f;
+    Preferences settingsPrefs;
+    SettingsView sv;
 
     public MainScreen(BitmapFont f1, BitmapFont f2) {
         font = f1;
@@ -53,19 +56,25 @@ public class MainScreen implements IFont, InputProcessor {
         bigDragon = new BigDragon();
         background = new Texture("bg.png");
         bigDragonFlap = Gdx.audio.newMusic(Gdx.files.internal("sounds/big_dragon_wing_flap.wav"));
+        settingsPrefs = Gdx.app.getPreferences("Global Settings");
         bigDragonFlap.setLooping(true);
+        bigDragonFlap.setVolume(settingsPrefs.getFloat("sound", 1.0f));
         bigDragonFlap.play();
+        sv = new SettingsView(font2);
         Gdx.input.setInputProcessor(this);
     }
 
     public void draw(SpriteBatch batch) {
         batch.draw(background, 0, -1, Gdx.graphics.getWidth(), Gdx.graphics.getHeight() + 1);
-        bigDragon.draw(batch);
-        drawFont("Dragon", batch, font, 1.0f, Gdx.graphics.getWidth()*0.5f, Gdx.graphics.getHeight()*0.9f);
-        drawFont("Defence", batch, font, 1.0f, Gdx.graphics.getWidth()*0.5f, Gdx.graphics.getHeight()*0.65f);
-        newGameButton.draw(batch);
-        settingsButton.draw(batch);
-        chooseButton.draw(batch);
+        if (!settings && !chooseLevel) {
+            bigDragon.draw(batch);
+            drawFont("Dragon", batch, font, 1.0f, Gdx.graphics.getWidth() * 0.5f, Gdx.graphics.getHeight() * 0.9f);
+            drawFont("Defence", batch, font, 1.0f, Gdx.graphics.getWidth() * 0.5f, Gdx.graphics.getHeight() * 0.65f);
+            newGameButton.draw(batch);
+            settingsButton.draw(batch);
+            chooseButton.draw(batch);
+        }
+        if (settings) sv.draw(batch);
     }
 
     public void dispose() {
@@ -74,6 +83,7 @@ public class MainScreen implements IFont, InputProcessor {
         chooseButton.dispose();
         font.dispose();
         background.dispose();
+        bigDragonFlap.dispose();
     }
 
     @Override
@@ -96,11 +106,57 @@ public class MainScreen implements IFont, InputProcessor {
         newGameButton.setState(newGameButton.getR().contains(screenX, Gdx.graphics.getHeight() - screenY));
         settingsButton.setState(settingsButton.getR().contains(screenX, Gdx.graphics.getHeight() - screenY));
         chooseButton.setState(chooseButton.getR().contains(screenX, Gdx.graphics.getHeight() - screenY));
+        if (settings) {
+            sv.backButton.setState(sv.backButton.getR().contains(screenX, Gdx.graphics.getHeight() - screenY));
+            if (sv.slider.contains(screenX, Gdx.graphics.getHeight() - screenY)) sv.sliderTouched = true;
+            if (sv.easy.contains(screenX, Gdx.graphics.getHeight() - screenY)) {
+                sv.e = true;
+                sv.m = false;
+                sv.h = false;
+                settingsPrefs.putInteger("Difficulty", 1);
+                settingsPrefs.flush();
+            }
+            if (sv.medium.contains(screenX, Gdx.graphics.getHeight() - screenY)) {
+                sv.e = false;
+                sv.m = true;
+                sv.h = false;
+                settingsPrefs.putInteger("Difficulty", 2);
+                settingsPrefs.flush();
+            }
+            if (sv.hard.contains(screenX, Gdx.graphics.getHeight() - screenY)) {
+                sv.e = false;
+                sv.m = false;
+                sv.h = true;
+                settingsPrefs.putInteger("Difficulty", 3);
+                settingsPrefs.flush();
+            }
+        }
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        newGameButton.setState(false);
+        settingsButton.setState(false);
+        chooseButton.setState(false);
+        if (settingsButton.getR().contains(screenX, Gdx.graphics.getHeight() - screenY) && !settings && !chooseLevel) {
+            settings = true;
+            bigDragonFlap.stop();
+        }
+        if (settings) {
+            if (sv.sliderTouched) {
+                sv.sliderTouched = false;
+                float vol = (sv.slider.x + sv.slider.width*0.5f - sv.r.x - sv.r.width*0.425f)/(sv.r.width*0.4f);
+                if (vol > 1) vol = 1f;
+                settingsPrefs.putFloat("sound", vol);
+                settingsPrefs.flush();
+            }
+            if (sv.backButton.getR().contains(screenX, Gdx.graphics.getHeight() - screenY)) {
+                bigDragonFlap.setVolume(settingsPrefs.getFloat("sound", 1.0f));
+                bigDragonFlap.play();
+                settings = false;
+            }
+        }
         return true;
     }
 
@@ -109,6 +165,11 @@ public class MainScreen implements IFont, InputProcessor {
         newGameButton.setState(newGameButton.getR().contains(screenX, Gdx.graphics.getHeight() - screenY));
         settingsButton.setState(settingsButton.getR().contains(screenX, Gdx.graphics.getHeight() - screenY));
         chooseButton.setState(chooseButton.getR().contains(screenX, Gdx.graphics.getHeight() - screenY));
+        if (settings) {
+            sv.backButton.setState(sv.backButton.getR().contains(screenX, Gdx.graphics.getHeight() - screenY));
+            if (sv.sliderTouched && screenX <= sv.r.x + sv.r.width*0.825f && screenX >= sv.r.x + sv.r.width*0.425f)
+                sv.slider.x = screenX;
+        }
         return true;
     }
 
---
+++ core/src/com/dragon/defence/td/screens/main/SettingsView.java
@@ -0,0 +1,90 @@
+package com.dragon.defence.td.screens.main;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.dragon.defence.td.Button;
+import com.dragon.defence.td.IFont;
+
+import java.nio.channels.spi.SelectorProvider;
+
+public class SettingsView implements IFont {
+    Rectangle r, slider, easy, medium, hard;
+    Texture bg, soundBarTexture, soundSliderTexture, unchecked, checked;
+    Button soundButton, difficultyButton, backButton;
+    boolean sliderTouched = false;
+    boolean e = false, m = true, h = false;
+    BitmapFont font;
+
+    public SettingsView(BitmapFont f) {
+        font = f;
+        r = new Rectangle(Gdx.graphics.getWidth()*0.15f,
+                Gdx.graphics.getHeight()*0.15f,
+                Gdx.graphics.getWidth()*0.7f,
+                Gdx.graphics.getHeight()*0.7f);
+        bg = new Texture("settings_bg.png");
+        soundBarTexture = new Texture("sound_bar.png");
+        soundSliderTexture = new Texture("sound_slider.png");
+        soundButton = new Button(new Texture("default_button.png"),
+                new Texture("pressed_button.png"),
+                new Rectangle(
+                        r.x + r.width*0.1f,
+                        r.y + r.height*0.8f - r.width*0.1f,
+                        r.width*0.3f,
+                        r.width*0.1f
+                ), "Sound", font, 0.2f);
+        difficultyButton = new Button(new Texture("default_button.png"),
+                new Texture("pressed_button.png"),
+                new Rectangle(
+                        r.x + r.width*0.1f,
+                        r.y + r.height*0.8f - r.width*0.2f,
+                        r.width*0.3f,
+                        r.width*0.1f
+                ), "Difficulty", font, 0.2f);
+        backButton = new Button(new Texture("default_button.png"),
+                new Texture("pressed_button.png"),
+                new Rectangle(
+                        r.x + r.width*0.395f,
+                        r.y + r.height*0.15f,
+                        r.width*0.21f,
+                        r.width*0.07f
+                ), "Back", font, 0.2f);
+        slider = new Rectangle(r.x + r.width*0.85f - r.width*0.025f,
+                r.y + r.height*0.8f - r.width*0.075f,
+                r.width*0.05f, r.width*0.05f);
+        easy = new Rectangle(r.x + r.width*0.5f,
+                r.y + r.height*0.8f - r.width*0.175f,
+                r.width*0.05f, r.width*0.05f);
+        medium = new Rectangle(r.x + r.width*0.655f,
+                r.y + r.height*0.8f - r.width*0.175f,
+                r.width*0.05f, r.width*0.05f);
+        hard = new Rectangle(r.x + r.width*0.813f,
+                r.y + r.height*0.8f - r.width*0.175f,
+                r.width*0.05f, r.width*0.05f);
+        checked = new Texture("checkbox_checked.png");
+        unchecked = new Texture("checkbox_empty.png");
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(bg, r.x, r.y, r.width, r.height);
+        soundButton.draw(batch);
+        backButton.draw(batch);
+        difficultyButton.draw(batch);
+        batch.draw(soundBarTexture, r.x + r.width*0.45f, r.y + r.height*0.8f - r.width*0.06f,
+                r.width*0.4f, r.width*0.02f);
+        batch.draw(soundSliderTexture, slider.x, slider.y, slider.width, slider.height);
+        drawFont("E:", batch, font, 0.2f, r.x + r.width*0.465f, r.y + r.height*0.8f - r.width*0.15f);
+        drawFont("M:", batch, font, 0.2f, r.x + r.width*0.61f, r.y + r.height*0.8f - r.width*0.15f);
+        drawFont("H:", batch, font, 0.2f, r.x + r.width*0.775f, r.y + r.height*0.8f - r.width*0.15f);
+        if (e) batch.draw(checked, easy.x, easy.y, easy.width, easy.height);
+        else batch.draw(unchecked, easy.x, easy.y, easy.width, easy.height);
+        if (m) batch.draw(checked, medium.x, medium.y, medium.width, medium.height);
+        else batch.draw(unchecked, medium.x, medium.y, medium.width, medium.height);
+        if (h) batch.draw(checked, hard.x, hard.y, hard.width, hard.height);
+        else batch.draw(unchecked, hard.x, hard.y, hard.width, hard.height);
+    }
+}
